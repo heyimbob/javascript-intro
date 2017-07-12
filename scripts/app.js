ea9c04264330f3f50ea801f64e9ddc27b0a4455f
Bob G <robertgiova@gmail.com>
2017-07-11 20:16:59 -0500

    Added first JS commit

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+window.onload = function(event) {
+	console.log('Hi mom');
+	console.log(document);
+	let ul = document.createElement('ul');
+	console.log(ul);
+
+	document.getElementById('list').innerHTML = 'I like cats';
+
+}
